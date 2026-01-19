38c25c7de628abe005033963b47fcf6042fc4c0e
EmekeChukwuka <chukzemeke9@gmail.com>
2026-01-19 10:41:26 +0100

    AddedRegexusernamevalidationtoCrapsgame

--- Craps-game/Craps-game.js
+++ Craps-game/Craps-game.js
@@ -5,12 +5,16 @@ const CrapsMainSection = "Craps-main-section"
 
 
 function registerPlayer() {
-	let Crapsusername = alert(document.getElementById("Craps-username-input").value);
-alert("Got: " + Crapsusername);
-removingRegistrationBox();
-showMainGameSection();
-}
+	let Crapsusername = document.getElementById("Craps-username-input").value;
 
+	//username validation check
+	let firstCharIsDigitRegex = /^[0-9]|[^a-zA-Z0-9_]/g
+	if (Crapsusername.length < 5 || firstCharIsDigitRegex.test(Crapsusername)) {
+		alert("Your character must be at least 5 characters long, alphanumeric only (a-z/A-Z/0-9), have no spaces and cannot start with a number!"); 
+	 } else {
+		removingRegistrationBox();
+		showMainGameSection();}
+	}
 
 function removingRegistrationBox() {
 document.getElementById("Craps-game-registration").style.display= "none";
