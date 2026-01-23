8f849d5dd0b0fed30d1f575d069047745202bcc1
EmekeChukwuka <chukzemeke9@gmail.com>
2026-01-23 10:16:53 +0100

    Nested loops in javascript

--- personalwebsite/hello.js
+++ personalwebsite/hello.js
@@ -1,36 +1,10 @@
-// Ex 1 - product by addition
-console.log('\nEXERCISE 1:\n');
-let multiplicand = 12;
-let multiplier = 4;
-let product = 0;
-for (let i = 0; i < multiplier; i++) {
-  product += multiplicand;
-}
-console.log('final product of ' + multiplicand + ' time ' + multiplier + ': ' + product)
+let rows =5;
 
-
-// Ex 2 - Sum of numbers from 1 to x
-console.log('\nEXERCISE 2:\n');
-let stopsum = 10
-let finalsum = 0;
-for (let i = 1; i <= stopsum; i++) {
-  finalsum += i;
-}
-console.log('final sum of numbers from 1 to ' + stopsum + ' is: ' + finalsum)
-
-// Ex 3 - Array element of longest string
-console.log('\nEXERCISE 3:\n');
-let stringArray = ['apple', 'banana', 'cherry', 'dragonfruit', 'elderberry', 'fig', 'grapefruit'];
-let longestString = '0'
-let maxlength = 0
-
-for (let i = 0; i < stringArray.length; i++) {
-  let elementlength = stringArray[i].length
-    if (elementlength > maxlength) {
-        longestString = i
-        maxlength = elementlength
+console.log("Number of rows:", rows);
+for (let row = 1; row <= rows; row++) {
+    let stars = '';
+    for (let col = 1; col <= row; col++) {
+        stars += '*';
     }
+    console.log(stars);
 }
-
-console.log(stringArray)
-console.log('The longest string is: ' + stringArray + ' which is' + maxlength)
