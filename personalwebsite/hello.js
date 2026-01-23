748f0ec6ccbad96b0cd9fb9e88b87cf09fe79242
EmekeChukwuka <chukzemeke9@gmail.com>
2026-01-23 10:42:32 +0100

    Loops practice

--- personalwebsite/hello.js
+++ personalwebsite/hello.js
@@ -1,10 +1,43 @@
-let rows =5;
+//let rows =5;
 
-console.log("Number of rows:", rows);
-for (let row = 1; row <= rows; row++) {
-    let stars = '';
-    for (let col = 1; col <= row; col++) {
-        stars += '*';
-    }
-    console.log(stars);
+//console.log("Number of rows:", rows);
+//for (let row = 1; row <= rows; row++) {
+//    let stars = '';
+//    for (let col = 1; col <= row; col++) {
+//        stars += '*';
+//    }
+//    console.log(stars);
+//}
+
+let originalString = "I am learning JavaScript";
+console.log('original:' + originalString);
+let reversedString = '';
+
+for (let character of originalString) {
+    reversedString = character + reversedString;
 }
+
+console.log(reversedString);
+
+
+let array = ['arrays', 'are', 'iterable'];
+let characterCount = {};
+
+console.log(array)
+
+for (let element of array) {
+    for (let character of element) {
+        if (character in characterCount) {
+          characterCount[character] += 1;
+        } else {
+            characterCount[character] = 1;
+        }
+      }
+}
+
+console.log(characterCount);
+
+
+
+
+
